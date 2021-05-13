c147c16d6f1633e269300e29b8daaa49d174f7d7
caoyus <caoyusong9527@163.com>
2021-05-13 12:55:26 +0800

    updated readonlymodifier_class.ts file

--- src/readonly_modifier.ts
+++ src/readonly_modifier.ts
@@ -1,1 +1,16 @@
 // readonly modifier
+// 首先它是一个关键字,它可以对类中的属性成员进行修饰,被修饰的成员就不会在外部被随意修改了
+
+class Stu {
+  readonly school: string;
+  constructor(school: string) {
+    this.school = school;
+  }
+
+  public changeSchool() {
+    //   this.school="anhui"//只读属性不能修改
+  }
+}
+
+const sh = new Stu("ShangHai School");
+sh.school;
